957c9ce8a76aa4df1cef3b99233302eca5fac30a
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-12-11 15:11:32 +0100

    Dodaj pliki projektów.

---
+++ MobileDevices_ProjectTask/MobileDevices_ProjectTask/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileDevices_ProjectTask
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Device.SetFlags(new[]
+            {
+                "RadioButton_Experimental",
+            });
+
+            MainPage = new NavigationPage(new MainPage()); ;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MobileDevices_ProjectTask/MobileDevices_ProjectTask/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileDevices_ProjectTask
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void PerformCheck(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void ReloadPage(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
