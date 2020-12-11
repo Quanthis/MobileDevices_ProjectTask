681f66663a6c9855df19bbc01da7e413d996e180
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-12-11 15:36:38 +0100

    Code --Added ContentLoader and FVWord classess in order to keep and check spelling words (check incoming)

---
+++ MobileDevices_ProjectTask/MobileDevices_ProjectTask/ContentLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileDevices_ProjectTask
+{
+    public class ContentLoader
+    {
+        private List<FVWord> fVWords;
+
+        public ContentLoader() 
+        {
+            fVWords = GenerateFVWords();
+        }
+
+        private List<FVWord> GenerateFVWords()
+        {
+            List <FVWord> fVWords = new List<FVWord>();
+
+            fVWords.Add(new FVWord("arise", "arose", "arisen"));
+            fVWords.Add(new FVWord("awake", "awoke", "awoken"));
+            fVWords.Add(new FVWord("become", "became", "become"));
+            fVWords.Add(new FVWord("begin", "began", "begun"));
+            fVWords.Add(new FVWord("break", "brokre", "broken"));
+            fVWords.Add(new FVWord("bring", "brought", "brought"));
+            fVWords.Add(new FVWord("build", "built", "built"));
+            fVWords.Add(new FVWord("buy", "bought", "bought"));
+            fVWords.Add(new FVWord("catch", "caught", "caught"));
+
+            return fVWords;
+        }
+    }
+}
---
+++ MobileDevices_ProjectTask/MobileDevices_ProjectTask/FVWord.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MobileDevices_ProjectTask
+{
+    public class FVWord  //FVWord - full varation word
+    {
+        public readonly string infinitive;
+        public readonly string pastTense;
+        public readonly string pastParticiple;
+
+        public enum WordGiven
+        {
+            infinitive,
+            pastTense,
+            pastParticiple
+        };
+
+        public FVWord(string invinitife, string pastTense, string pastParticiple)
+        {
+            this.infinitive = invinitife;
+            this.pastTense = pastTense;
+            this.pastParticiple = pastParticiple;
+        }
+
+        public async Task<bool> CheckIfWordSpelledCorrectly(WordGiven wordGiven, string inputForm1, string inputForm2)
+        {
+            return await Task.Run(()=>
+            {
+                switch (wordGiven)
+                {
+                    case WordGiven.infinitive:
+
+                        if (pastTense == inputForm1 && pastParticiple == inputForm2)
+                        {
+                            return true;
+                        }
+                        else return false;
+
+                    case WordGiven.pastTense:
+
+                        if (infinitive == inputForm1 && pastParticiple == inputForm2)
+                        {
+                            return true;
+                        }
+                        else return false;
+
+                    case WordGiven.pastParticiple:
+
+                        if (infinitive == inputForm1 && pastTense == inputForm2)
+                        {
+                            return true;
+                        }
+                        else return false;
+
+                    default: return false;
+                }
+            });
+        }
+    }
+}
